71422be6e85d46761f8db9d6e202bcb84261c4e2
sahel sh <sahelmail1@gmail.com>
2022-07-13 13:28:36 +0430

    redux commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+const store = createStore(reducer, applyMiddleware(thunk));
+import {reducer} from './redux/reducer'
+import thunk from "redux-thunk";
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+
+      <App />
+
   </Provider>,
-  document.getElementById("root")
+  rootElement
 );
 
-// If you want your app to work offline and load faster, you can chađinge
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,153 +1,142 @@
-import React, { useState, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+// import React, { useState, useRef } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect } from 'react-router-dom';
+// import { connect } from 'react-redux'
 
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
+// import Form from "react-validation/build/form";
+// import Input from "react-validation/build/input";
+// import CheckButton from "react-validation/build/button";
 
-import { login } from "src/actions/auth";
+// import { login } from "src/actions/auth";
 
-const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
-};
-
-const Login = (props) => {
-  const form = useRef();
-  const checkBtn = useRef();
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const { isLoggedIn } = useSelector(state => state.auth);
-  const { message } = useSelector(state => state.message);
-
-  const dispatch = useDispatch();
-
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-  };
-
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    setLoading(true);
-
-    form.current.validateAll();
-
-    if (checkBtn.current.context._errors.length === 0) {
-      dispatch(login(username, password))
-        .then(() => {
-          props.history.push("/profile");
-          window.location.reload();
-        })
-        .catch(() => {
-          setLoading(false);
-        });
-    } else {
-      setLoading(false);
-    }
-  };
-
-  // if (isLoggedIn) {
-  //   return <Redirect to="/profile" />;
-  // }
-
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-
-        <Form onSubmit={handleLogin} ref={form}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <Input
-              type="text"
-              className="form-control"
-              name="username"
-              value={username}
-              onChange={onChangeUsername}
-              validations={[required]}
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={onChangePassword}
-              validations={[required]}
-            />
-          </div>
-
-          <div className="form-group">
-            <button  className="btn btn-primary btn-block" disabled={loading}>
-            {this.props.redisterSuccess}
-              {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Login</span>
-             
-            </button>
-          </div>
-
-          {message && (
-            <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-      </div>
-    </div>
-  );
-};
-
-//  export default Login;
-const mapStateToProps = (state) => {
-  return{ctr: state.isLoggedIn}
-  
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    redisterSuccess: () => dispatch({ type: 'REGISTER_SUCCESS' }),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
-
-// function mapState(state) {
-//   const { loggingIn } = state.auth;
-//   return { loggingIn };
-// }
-
-// const actionCreators = {
-//   login: userActions.login,
+// const required = (value) => {
+//   if (!value) {
+//     return (
+//       <div className="alert alert-danger" role="alert">
+//         This field is required!
+//       </div>
+//     );
+//   }
 // };
 
-// const connectedLoginPage = connect(mapState, actionCreators)(Login);
-// export { connectedLoginPage as Login};
+// const Login = (props) => {
+//   const form = useRef();
+//   const checkBtn = useRef();
+
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [loading, setLoading] = useState(false);
+
+//   const { isLoggedIn } = useSelector(state => state.auth);
+//   const { message } = useSelector(state => state.message);
+
+//   const dispatch = useDispatch();
+
+//   const onChangeUsername = (e) => {
+//     const username = e.target.value;
+//     setUsername(username);
+//   };
+
+//   const onChangePassword = (e) => {
+//     const password = e.target.value;
+//     setPassword(password);
+//   };
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+
+//     setLoading(true);
+
+//     form.current.validateAll();
+
+//     if (checkBtn.current.context._errors.length === 0) {
+//       dispatch(login(username, password))
+//         .then(() => {
+//           props.history.push("/profile");
+//           window.location.reload();
+//         })
+//         .catch(() => {
+//           setLoading(false);
+//         });
+//     } else {
+//       setLoading(false);
+//     }
+//   };
+
+//   // if (isLoggedIn) {
+//   //   return <Redirect to="/profile" />;
+//   // }
+
+//   return (
+//     <div className="col-md-12">
+//       <div className="card card-container">
+//         <img
+//           src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+//           alt="profile-img"
+//           className="profile-img-card"
+//         />
+
+//         <Form onSubmit={handleLogin} ref={form}>
+//           <div className="form-group">
+//             <label htmlFor="username">Username</label>
+//             <Input
+//               type="text"
+//               className="form-control"
+//               name="username"
+//               value={username}
+//               onChange={onChangeUsername}
+//               validations={[required]}
+//             />
+//           </div>
+
+//           <div className="form-group">
+//             <label htmlFor="password">Password</label>
+//             <Input
+//               type="password"
+//               className="form-control"
+//               name="password"
+//               value={password}
+//               onChange={onChangePassword}
+//               validations={[required]}
+//             />
+//           </div>
+
+//           <div className="form-group">
+//             <button  className="btn btn-primary btn-block" disabled={loading}>
+//             {this.props.redisterSuccess}
+//               {loading && (
+//                 <span className="spinner-border spinner-border-sm"></span>
+//               )}
+//               <span>Login</span>
+             
+//             </button>
+//           </div>
+
+//           {message && (
+//             <div className="form-group">
+//               <div className="alert alert-danger" role="alert">
+//                 {message}
+//               </div>
+//             </div>
+//           )}
+//           <CheckButton style={{ display: "none" }} ref={checkBtn} />
+//         </Form>
+//       </div>
+//     </div>
+//   );
+// };
+
+//   export default Login;
+
+
+// // function mapState(state) {
+// //   const { loggingIn } = state.auth;
+// //   return { loggingIn };
+// // }
+
+// // const actionCreators = {
+// //   login: userActions.login,
+// // };
+
+// // const connectedLoginPage = connect(mapState, actionCreators)(Login);
+// // export { connectedLoginPage as Login};
